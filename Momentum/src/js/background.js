c082d4c14767d91b32a70cf9e528f837155b42e0
Hyobae Lee <lhb0803@snu.ac.kr>
2022-06-19 12:53:59 +0900

    decorate

--- Momentum/src/js/background.js
+++ Momentum/src/js/background.js
@@ -1,14 +1,20 @@
+const imgBox = document.querySelector("#img-box");
+
 const images = [
     "0.jpeg",
     "1.jpeg",
-    "2.jpeg"
+    "2.jpeg",
+    "3.jpeg",
+    "4.jpeg",
+    "5.jpeg",
+    "6.jpeg"
 ];
 const imageLength = images.length;
 
 const chosenImage = images[Math.floor(Math.random() * imageLength)];
 
-const bgImage = document.createElement("img");
+const bgImage = imgBox.querySelector("img");
 bgImage.src = `src/img/${chosenImage}`;
 
-// add the element to the body
-document.body.appendChild(bgImage);
+// add the element to the imgBox
+imgBox.appendChild(bgImage);
